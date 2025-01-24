6c06c451f4fc9a1790c005300ba987358047c018
Nathan Jarod Crutchfield <njcrutchfield@i16n1.ocelote.hpc.arizona.edu>
2025-01-24 11:59:36 -0700

    modified the directories and updated gitignore

--- torch/NN/AutoEncoderCNN.py
+++ torch/NN/AutoEncoderCNN.py
@@ -1,112 +1,165 @@
 import torch
-class AE_CNN(torch.nn.Module):
-    def __init__(self, encoded_dim:int=2048, rand_mat_dim:int=1024, rand_mat = True) -> None:
+import torch.nn as nn
+class AE(torch.nn.Module):
+    def __init__(self, encoder, decoder, rand_mat):
         super().__init__()
+        self.encoder = encoder
+        self.decoder = decoder
+        self.rand_mat = rand_mat
         
-        self.encoded_vector = None
+    def forward(self, x):
+        enc = encoder(x)
         
-        if rand_mat: 
-            self.rand_mat = self.create_rand_mat(rand_mat_dim, encoded_dim)
-        else:
-            self.rand_mat = torch.randn(rand_mat_dim, encoded_dim, requires_grad=False, device='cuda' if torch.cuda.is_available() else 'cpu') # dummy rand matrix of correct dim
-        
-        self.softmax = torch.nn.Softmax()
-        
-        self.encoder = torch.nn.Sequential(
-            torch.nn.Conv2d(3,16,3,stride=1,padding=1), # Outputs: 299 x 299 x 16
-            torch.nn.ReLU(),
-            torch.nn.Conv2d(16,32,3,stride=1,padding=1), # Outputs: 299 x 299 x 32
-            torch.nn.ReLU(),
-            
-            torch.nn.AvgPool2d(2,stride=2, padding=1), # Outputs: 150 x 150 x 32
-            torch.nn.Conv2d(32,64,3,stride=1,padding=1), # Outputs: 150 x 150 x 64
-            torch.nn.ReLU(),
-            
-            torch.nn.AvgPool2d(2,stride=2, padding=1), # Outputs: 76 x 76 x 64
-            torch.nn.Conv2d(64,128,3,stride=1,padding=1), # Outputs: 76 x 76 x 128
-            torch.nn.ReLU(),
-            
-            torch.nn.AvgPool2d(2,stride=2, padding=1), # Outputs: 39 x 39 x 128
-            torch.nn.Conv2d(128,320,3,stride=1,padding=1), # Outputs: 39 x 39 x 320
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(320,240,3,stride=1,padding=1), # Outputs: 39 x 39 x 240
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(240,150,3,stride=1,padding=1), # Outputs: 39 x 39 x 150
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(150,80,3,stride=2,padding=1), # Outputs: 20 x 20 x 80
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(80,40,3,stride=1,padding=1), # Outputs: 20 x 20 x 40
-            torch.nn.ReLU(),
-            
-            torch.nn.Conv2d(40,15,3,stride=1,padding=1), # Outputs: 20 x 20 x 15
-            torch.nn.ReLU(),
-            
-            
-            torch.nn.Flatten(),
-            torch.nn.Linear(20*20*15, encoded_dim)
-        )
-        
-        self.decoder = torch.nn.Sequential(
-            torch.nn.Linear(rand_mat_dim, 20*20*15), # rand_mat_dim -> flattened dim
-            torch.nn.Unflatten(1, (15, 20, 20)),
-            
-            torch.nn.ConvTranspose2d(15,40,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(40,80,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(80,150,3, stride=2, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(150,240,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(240,320,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(320,128,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(128,128,3, stride=2, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(128,64,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(64,64,3, stride=2, padding=2), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(64,32,3, stride=1, padding=2), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(32,32,3, stride=2, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(32,16,3, stride=1, padding=1), 
-            torch.nn.ReLU(),
-            
-            torch.nn.ConvTranspose2d(16,3,3, stride=1, padding=0), 
-            torch.nn.Sigmoid()
-        )
-        
-    def create_rand_mat(self, rand_mat_dim:int, encoded_dim:int) -> torch.tensor:
-        y = torch.randn(rand_mat_dim, encoded_dim, requires_grad=False, device='cuda' if torch.cuda.is_available() else 'cpu')
-        norm = torch.norm(y, dim = 0)
-        for col in range(len(y[0])):
-            for row in range(len(y)):
-                y[row][col] /= norm[col]
-        return y
-        
-    def forward(self, x) -> torch.tensor:
-        self.encoded_vector = self.softmax(self.encoder(x))
         all_dps = []
-        for batch in self.encoded_vector:
+        for batch in enc:
             all_dps.append(torch.mv(self.rand_mat, batch))
         Dp = torch.stack(all_dps)
-        decoded = self.decoder(Dp)
-        return decoded
+        
+        p_vec = F.softmax(Dp, dim = 1)
+        
+        
+        
+        dec = decoder(p_vec)
+        return dec, p_vec
+    
+    
+
+class Encoder(nn.Module):
+    def __init__(self, in_channel=3, out_channel=3500, activation=nn.GELU()):
+        super().__init__()
+        # The input size is 3*299*299
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+        self.encoder = nn.Sequential(
+                                nn.Conv2d(in_channels=in_channel,out_channels=16,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(16), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=16,out_channels=32,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(32), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=32,out_channels=32,kernel_size=2,stride=2,padding=1,bias=False),
+                                nn.BatchNorm2d(32), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=32,out_channels=64,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(64), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=64,out_channels=64,kernel_size=2,stride=2,padding=1,bias=False),
+                                nn.BatchNorm2d(64), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=64,out_channels=128,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(128), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=128,out_channels=128,kernel_size=2,stride=2,padding=1,bias=False),
+                                nn.BatchNorm2d(128), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=128,out_channels=320,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(320), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=320,out_channels=240,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(240), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=240,out_channels=150,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(150), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=150,out_channels=80,kernel_size=3,stride=2,padding=1,bias=False),
+                                nn.BatchNorm2d(80), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=80,out_channels=40,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(40), 
+                                activation,
+            
+                                nn.Conv2d(in_channels=40,out_channels=15,kernel_size=3,stride=1,padding=1,bias=False),
+                                nn.BatchNorm2d(15), 
+                                activation,
+            
+                                nn.Flatten(),
+                                nn.Linear(in_features=15*20*20, out_features=out_channel),
+                                nn.Softmax(dim=1))
+        
+    def forward(self, x):
+        x = x.view(-1, self.in_channel, 299, 299)
+        prob = self.encoder(x)
+        return prob
+
+
+class Decoder(nn.Module):
+    def __init__(self, in_channel=3, out_channel=1600, activation=nn.GELU()):
+        super().__init__()
+        self.in_channel = in_channel
+        self.out_channel = out_channel
+
+        self.mlp = nn.Sequential(
+                             nn.Linear(in_features=out_channel, out_features=15*20*20, bias=False),
+                             nn.BatchNorm1d(20*20*15),
+                             activation,
+                             nn.Unflatten(1, (15, 20, 20)))
+        
+        self.decoder = nn.Sequential(
+                                nn.ConvTranspose2d(in_channels=15,out_channels=40,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(40), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=40,out_channels=80,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(80), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=80,out_channels=150,kernel_size=3,stride=2,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(150), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=150,out_channels=240,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(240), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=240,out_channels=320,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(320), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=320,out_channels=128,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(128), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=128,out_channels=128,kernel_size=3,stride=2,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(128), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=128,out_channels=64,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(64), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=64,out_channels=64,kernel_size=3,stride=2,padding=2,output_padding=0,bias=False),
+                                nn.BatchNorm2d(64), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=64,out_channels=32,kernel_size=3,stride=1,padding=2,output_padding=0,bias=False),
+                                nn.BatchNorm2d(32), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=32,out_channels=32,kernel_size=3,stride=2,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(32), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=32,out_channels=16,kernel_size=3,stride=1,padding=1,output_padding=0,bias=False),
+                                nn.BatchNorm2d(16), 
+                                activation,
+            
+                                nn.ConvTranspose2d(in_channels=16, out_channels=in_channel, kernel_size=3, stride=1, padding=0, output_padding=0),
+                                nn.Sigmoid())
+    
+    def forward(self, x):
+        x = x.view(-1, self.out_channel)
+        temp = self.mlp(x)
+        temp = temp.view(-1,15,20,20)
+        output = self.decoder(temp)
+        return output
